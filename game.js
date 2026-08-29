e97c7cb1b6f0fdfa3a02d140790bd9cfecab4f05
Ramon Villalvazo <rvillalvazo@gmail.com>
2026-08-28 19:34:00 -0600

    Nueva pieza Tuerca

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ const COLORS = [
   '#e57373', // Z - red
   '#90caf9', // J - pale blue
   '#ffb74d', // L - orange
+  '#b0bec5', // NUT - metallic gray
 ];
 
 const PIECES = [
@@ -24,6 +25,7 @@ const PIECES = [
   [[5,5,0],[0,5,5],[0,0,0]],                  // Z
   [[6,0,0],[6,6,6],[0,0,0]],                  // J
   [[0,0,7],[7,7,7],[0,0,0]],                  // L
+  [[8,8,8],[8,0,8],[8,8,8]],                  // NUT - hueco central
 ];
 
 const LINE_SCORES = [0, 100, 300, 500, 800];
@@ -51,7 +53,8 @@ function createBoard() {
 }
 
 function randomPiece() {
-  const type = Math.floor(Math.random() * 7) + 1;
+  const NUT_CHANCE = 0.06;
+  const type = Math.random() < NUT_CHANCE ? 8 : Math.floor(Math.random() * 7) + 1;
   const shape = PIECES[type].map(row => [...row]);
   return { type, shape, x: Math.floor(COLS / 2) - Math.floor(shape[0].length / 2), y: 0 };
 }
